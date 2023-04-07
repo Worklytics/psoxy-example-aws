448f0767c6a5e159277ff2fe086d9142b645b891
Erik Schultink <erik@worklytics.co>
2023-04-07 14:26:27 -0700

    update to reference latest release

--- main.tf
+++ main.tf
@@ -57,7 +57,7 @@ provider "azuread" {
 
 module "psoxy" {
   # source = "../psoxy/infra/modular-examples/aws"
-  source = "git::https://github.com/worklytics/psoxy//infra/modular-examples/aws?ref=v0.4.17"
+  source = "git::https://github.com/worklytics/psoxy//infra/modular-examples/aws?ref=v0.4.18"
 
   aws_account_id                 = var.aws_account_id
   aws_assume_role_arn            = var.aws_assume_role_arn # role that can test the instances (lambdas)
