33826ba587714d1ab82f21858f7e1b3f539dd4f5
Erik Schultink <erik@worklytics.co>
2023-06-30 07:10:05 -0700

    Update example to v0.4.27

--- main.tf
+++ main.tf
@@ -31,7 +31,7 @@ terraform {
 
 # general cases
 module "worklytics_connectors" {
-  source = "git::https://github.com/worklytics/psoxy//infra/modules/worklytics-connectors?ref=v0.4.26"
+  source = "git::https://github.com/worklytics/psoxy//infra/modules/worklytics-connectors?ref=v0.4.27"
 
   enabled_connectors    = var.enabled_connectors
   example_jira_issue_id = var.example_jira_issue_id
@@ -98,7 +98,7 @@ locals {
 }
 
 module "psoxy" {
-  source = "git::https://github.com/worklytics/psoxy//infra/modules/aws-host?ref=v0.4.26"
+  source = "git::https://github.com/worklytics/psoxy//infra/modules/aws-host?ref=v0.4.27"
 
   environment_name               = var.environment_name
   aws_account_id                 = var.aws_account_id
@@ -137,7 +137,7 @@ locals {
 module "connection_in_worklytics" {
   for_each = local.all_instances
 
-  source = "git::https://github.com/worklytics/psoxy//infra/modules/worklytics-psoxy-connection-generic?ref=v0.4.26"
+  source = "git::https://github.com/worklytics/psoxy//infra/modules/worklytics-psoxy-connection-generic?ref=v0.4.27"
 
   psoxy_host_platform_id = local.host_platform_id
   psoxy_instance_id      = each.key
