37637023dd625d35fb6b510436a46d4f31a0d6a3
Erik Schultink <erik@worklytics.co>
2023-08-16 14:16:19 -0700

    Update example to v0.4.35

--- main.tf
+++ main.tf
@@ -31,16 +31,17 @@ terraform {
 
 # general cases
 module "worklytics_connectors" {
-  source = "git::https://github.com/worklytics/psoxy//infra/modules/worklytics-connectors?ref=v0.4.31"
+  source = "git::https://github.com/worklytics/psoxy//infra/modules/worklytics-connectors?ref=v0.4.35"
 
-  enabled_connectors        = var.enabled_connectors
-  jira_cloud_id             = var.jira_cloud_id
-  jira_server_url           = var.jira_server_url
-  jira_example_issue_id     = var.jira_example_issue_id
-  salesforce_domain         = var.salesforce_domain
-  github_installation_id    = var.github_installation_id
-  github_organization       = var.github_organization
-  github_example_repository = var.github_example_repository
+  enabled_connectors            = var.enabled_connectors
+  jira_cloud_id                 = var.jira_cloud_id
+  jira_server_url               = var.jira_server_url
+  jira_example_issue_id         = var.jira_example_issue_id
+  salesforce_domain             = var.salesforce_domain
+  github_installation_id        = var.github_installation_id
+  github_organization           = var.github_organization
+  github_example_repository     = var.github_example_repository
+  salesforce_example_account_id = var.salesforce_example_account_id
 }
 
 # sources which require additional dependencies are split into distinct Terraform files, following
@@ -101,31 +102,32 @@ locals {
 }
 
 module "psoxy" {
-  source = "git::https://github.com/worklytics/psoxy//infra/modules/aws-host?ref=v0.4.31"
+  source = "git::https://github.com/worklytics/psoxy//infra/modules/aws-host?ref=v0.4.35"
 
-  environment_name               = var.environment_name
-  aws_account_id                 = var.aws_account_id
-  aws_ssm_param_root_path        = var.aws_ssm_param_root_path
-  psoxy_base_dir                 = var.psoxy_base_dir
-  deployment_bundle              = var.deployment_bundle
-  install_test_tool              = var.install_test_tool
-  provision_testing_infra        = var.provision_testing_infra
-  force_bundle                   = var.force_bundle
-  caller_gcp_service_account_ids = var.caller_gcp_service_account_ids
-  caller_aws_arns                = var.caller_aws_arns
-  non_production_connectors      = var.non_production_connectors
-  custom_api_connector_rules     = var.custom_api_connector_rules
-  lookup_table_builders          = var.lookup_table_builders
-  general_environment_variables  = var.general_environment_variables
-  function_env_kms_key_arn       = var.project_aws_kms_key_arn
-  logs_kms_key_arn               = var.project_aws_kms_key_arn
-  aws_ssm_key_id                 = var.project_aws_kms_key_arn
-  bulk_sanitized_expiration_days = var.bulk_sanitized_expiration_days
-  bulk_input_expiration_days     = var.bulk_input_expiration_days
-  api_connectors                 = local.api_connectors
-  bulk_connectors                = local.bulk_connectors
-  custom_bulk_connector_rules    = var.custom_bulk_connector_rules
-  todo_step                      = local.max_auth_todo_step
+  environment_name                = var.environment_name
+  aws_account_id                  = var.aws_account_id
+  aws_ssm_param_root_path         = var.aws_ssm_param_root_path
+  psoxy_base_dir                  = var.psoxy_base_dir
+  deployment_bundle               = var.deployment_bundle
+  install_test_tool               = var.install_test_tool
+  provision_testing_infra         = var.provision_testing_infra
+  force_bundle                    = var.force_bundle
+  caller_gcp_service_account_ids  = var.caller_gcp_service_account_ids
+  caller_aws_arns                 = var.caller_aws_arns
+  non_production_connectors       = var.non_production_connectors
+  custom_api_connector_rules      = var.custom_api_connector_rules
+  lookup_table_builders           = var.lookup_table_builders
+  general_environment_variables   = var.general_environment_variables
+  function_env_kms_key_arn        = var.project_aws_kms_key_arn
+  logs_kms_key_arn                = var.project_aws_kms_key_arn
+  aws_ssm_key_id                  = var.project_aws_kms_key_arn
+  bulk_sanitized_expiration_days  = var.bulk_sanitized_expiration_days
+  bulk_input_expiration_days      = var.bulk_input_expiration_days
+  api_connectors                  = local.api_connectors
+  bulk_connectors                 = local.bulk_connectors
+  custom_bulk_connector_rules     = var.custom_bulk_connector_rules
+  custom_bulk_connector_arguments = var.custom_bulk_connector_arguments
+  todo_step                       = local.max_auth_todo_step
 }
 
 ## Worklytics connection configuration
@@ -140,7 +142,7 @@ locals {
 module "connection_in_worklytics" {
   for_each = local.all_instances
 
-  source = "git::https://github.com/worklytics/psoxy//infra/modules/worklytics-psoxy-connection-generic?ref=v0.4.31"
+  source = "git::https://github.com/worklytics/psoxy//infra/modules/worklytics-psoxy-connection-generic?ref=v0.4.35"
 
   psoxy_host_platform_id = local.host_platform_id
   psoxy_instance_id      = each.key
@@ -180,3 +182,12 @@ output "todos_3" {
   description = "List of todo steps to complete 3rd, in markdown format."
   value       = var.todos_as_outputs ? join("\n", values(module.connection_in_worklytics)[*].todo) : null
 }
+
+# although should be sensitive such that Terraform won't echo it to command line or expose it, leave
+# commented out in example until needed
+# if you uncomment it, you will then be able to obtain the value through `terraform output --raw pseudonym_salt`
+#output "pseudonym_salt" {
+#  description = "Value used to salt pseudonyms (SHA-256) hashes. If migrate to new deployment, you should copy this value."
+#  value       = module.psoxy.pseudonym_salt
+#  sensitive   = true
+#}
