63ccfa7bbe4a6cbd40353ba1edbd59155e3ac576
Erik Schultink <eschultink@users.noreply.github.com>
2023-12-06 13:23:27 -0800

    Update example to v0.4.42

--- google-workspace.tf
+++ google-workspace.tf
@@ -7,7 +7,7 @@ provider "google" {
 
 
 module "worklytics_connectors_google_workspace" {
-  source = "git::https://github.com/worklytics/psoxy//infra/modules/worklytics-connectors-google-workspace?ref=v0.4.41"
+  source = "git::https://github.com/worklytics/psoxy//infra/modules/worklytics-connectors-google-workspace?ref=v0.4.42"
 
   providers = {
     google = google.google_workspace
--- main.tf
+++ main.tf
@@ -18,7 +18,7 @@ terraform {
 
 # general cases
 module "worklytics_connectors" {
-  source = "git::https://github.com/worklytics/psoxy//infra/modules/worklytics-connectors?ref=v0.4.41"
+  source = "git::https://github.com/worklytics/psoxy//infra/modules/worklytics-connectors?ref=v0.4.42"
 
   enabled_connectors            = var.enabled_connectors
   jira_cloud_id                 = var.jira_cloud_id
@@ -94,7 +94,7 @@ locals {
 }
 
 module "psoxy" {
-  source = "git::https://github.com/worklytics/psoxy//infra/modules/aws-host?ref=v0.4.41"
+  source = "git::https://github.com/worklytics/psoxy//infra/modules/aws-host?ref=v0.4.42"
 
   environment_name                = var.environment_name
   aws_account_id                  = var.aws_account_id
@@ -134,7 +134,7 @@ locals {
 module "connection_in_worklytics" {
   for_each = local.all_instances
 
-  source = "git::https://github.com/worklytics/psoxy//infra/modules/worklytics-psoxy-connection-aws?ref=v0.4.41"
+  source = "git::https://github.com/worklytics/psoxy//infra/modules/worklytics-psoxy-connection-aws?ref=v0.4.42"
 
   psoxy_instance_id  = each.key
   worklytics_host    = var.worklytics_host
