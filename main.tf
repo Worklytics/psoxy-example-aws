d4ad4c826f216d91cd4a4d8aabf3a49cbc1c3463
Erik Schultink <eschultink@users.noreply.github.com>
2025-05-23 11:43:11 -0700

    bump version numbers

--- main.tf
+++ main.tf
@@ -1,5 +1,5 @@
 terraform {
-  required_version = ">= 1.3, < 1.10"
+  required_version = ">= 1.3, < 1.11"
 
   required_providers {
     # for the infra that will host Psoxy instances
@@ -20,7 +20,7 @@ terraform {
 
 # general cases
 module "worklytics_connectors" {
-  source = "git::https://github.com/worklytics/psoxy//infra/modules/worklytics-connectors?ref=v0.4.61"
+  source = "git::https://github.com/worklytics/psoxy//infra/modules/worklytics-connectors?ref=v0.4.62"
 
   enabled_connectors               = var.enabled_connectors
   jira_cloud_id                    = var.jira_cloud_id
@@ -99,7 +99,7 @@ locals {
 }
 
 module "psoxy" {
-  source = "git::https://github.com/worklytics/psoxy//infra/modules/aws-host?ref=v0.4.61"
+  source = "git::https://github.com/worklytics/psoxy//infra/modules/aws-host?ref=v0.4.62"
 
   environment_name                     = var.environment_name
   aws_account_id                       = var.aws_account_id
@@ -156,7 +156,7 @@ locals {
 module "connection_in_worklytics" {
   for_each = local.all_instances
 
-  source = "git::https://github.com/worklytics/psoxy//infra/modules/worklytics-psoxy-connection-aws?ref=v0.4.61"
+  source = "git::https://github.com/worklytics/psoxy//infra/modules/worklytics-psoxy-connection-aws?ref=v0.4.62"
 
   psoxy_instance_id    = each.key
   worklytics_host      = var.worklytics_host
